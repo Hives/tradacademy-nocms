1bbcd1fea263abe1251244997a386e8d535d0f4b
Hives <communty.hivemind@gmail.com>
2016-03-16 01:26:17 +0000

    added site navigation and a very basic 'wireframey' layout

--- tradacademy/courses/urls.py
+++ tradacademy/courses/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.courses_list),
-    url(r'^tutors/$', views.tutor_list),
+    url(r'^$',
+        views.courses_list,
+        name="courses-courses"
+        ),
+    url(r'^tutors/$',
+        views.tutor_list,
+        name="courses-tutors"
+        ),
     url(r'^tutors/(?P<tutor_slug>[\w\-]+)/$',
         views.tutor,
         name="courses-tutor"
